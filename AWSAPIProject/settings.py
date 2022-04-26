ac284c1325b8494e4f0b9cec4582a40f97c27158
JohnCaceres <43764825+JohnCaceres@users.noreply.github.com>
2022-04-26 01:58:11 -0500

    Main Project Generation
    Generation of the main project
    -added Urls for the RestAPI app

--- AWSAPIProject/settings.py
+++ AWSAPIProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'restAPI',
+
 ]
 
 MIDDLEWARE = [
@@ -113,6 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+APPEND_SLASH = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
